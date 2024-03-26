acdb8cc7c46c4aeea40ff97fb30d5b5d4615e3d1
hbelliston <a02391153@usu.edu>
2024-03-26 11:53:24 -0600

    Assignment 9 Update
    js adding

---
+++ javajam/common.js
@@ -0,0 +1,5 @@
+$(document).ready(function () {
+    $('#mug').click(function () {
+        alert("JavaJam Mug Club Members get a 10% discount on each cup of coffee!");
+    });
+});
---
+++ javajam/dateupdate.js
@@ -0,0 +1,1 @@
+document.getElementById("dateupdate").innerHTML = "Page Updated " + document.lastModified;
